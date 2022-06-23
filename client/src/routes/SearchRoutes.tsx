6d1168c053c19fe8a7515936e2cd91d49d73ced4
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-06-23 14:22:08 +0530

    bug fix

--- client/src/routes/SearchRoutes.tsx
+++ client/src/routes/SearchRoutes.tsx
@@ -53,7 +53,7 @@ const SearchRoutes: React.FC = () => {
   return <>
     {!loading ?
       <Routes>
-        <Route path="/" element={!user.user && currentUser ? <Login /> : <HomePage />} />
+        <Route path="/" element={!currentUser && !user.user ? <Login /> : <HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile/:useremail" element={<ProfiePage />} />
         <Route path="*" element={<Navigate replace to="/" />} />
