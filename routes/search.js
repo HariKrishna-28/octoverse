190c4ce17ac5b9dc3a69a7fb53999aca6ce6ab24
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-06-25 21:48:19 +0530

    Add universal search

---
+++ routes/search.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const query = req.query.q;
+    const result = await User.aggregate([
+      {
+        $search: {
+          autocomplete: {
+            query: query,
+            path: "userName",
+            fuzzy: {
+              maxEdits: 2,
+            },
+          },
+        },
+      },
+    ]);
+    const refinedUserData = result.map((user) => {
+      return {
+        _id: user._id,
+        userName: user.userName,
+        profilePicture: user.profilePicture,
+        email: user.email,
+      };
+    });
+    res.status(200).json(refinedUserData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
