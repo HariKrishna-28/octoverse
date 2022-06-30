8ba2010cd4a1e919e0cd8e0ac28cc281da3e4156
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-06-30 21:48:16 +0530

    add activity

---
+++ client/src/api/activityAPI.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosRequestConfig } from "axios";
+const act = `${process.env.REACT_APP_API_URL}/activity`;
+
+export function createNewActivity(activity: Object) {
+  const config: AxiosRequestConfig = {
+    method: "get",
+    url: `${act}/new`,
+    data: activity,
+  };
+  return axios(config);
+}
