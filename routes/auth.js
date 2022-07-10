61b7d0147958203b1cab52390bea59999f23e353
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-07-10 19:26:17 +0530

    remove register endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -3,52 +3,42 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 // REGISTER USER
-router.post("/register", async (req, res) => {
-  try {
-    //   GENERATE NEW ENCRYPTED PASSWORD
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+// router.post("/register", async (req, res) => {
+//   try {
+//     //   GENERATE NEW ENCRYPTED PASSWORD
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    // CREATE NEW USER
-    const newUser = new User({
-      userName: req.body.userName,
-      email: req.body.email,
-      password: hashedPassword,
-      description: req.body.description,
-      city: req.body.city,
-      from: req.body.from,
-    });
+//     // CREATE NEW USER
+//     const newUser = new User({
+//       userName: req.body.userName,
+//       email: req.body.email,
+//       password: hashedPassword,
+//       description: req.body.description,
+//       city: req.body.city,
+//       from: req.body.from,
+//     });
 
-    // SAVE USER
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({
-      status: "failed",
-      message: error,
-    });
-  }
-});
+//     // SAVE USER
+//     const user = await newUser.save();
+//     res.status(200).json(user);
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "failed",
+//       message: error,
+//     });
+//   }
+// });
 
 // LOGIN
 router.post("/login", async (req, res) => {
   try {
     const userCheck = await User.findOne({ email: req.body.email });
     if (!userCheck) {
-      // console.log(req.body);
       const newUser = new User(req.body);
       const user = await newUser.save();
       res.status(200).json(user);
     } else res.status(200).json(userCheck);
-    // else {
-    //   const validPassword = await bcrypt.compare(
-    //     req.body.password,
-    //     user.password
-    //   );
-
-    //   if (!validPassword) res.status(400).json({ message: "Wrong password" });
-    //   else res.status(200).json(user);
-    // }
   } catch (error) {
     console.log(error);
     res.status(500).json({
