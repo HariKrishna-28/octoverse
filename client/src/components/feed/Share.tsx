ac90d8d99a9c0f811a45acd2eb9a3e42709aa890
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-07-11 20:08:21 +0530

    fix login bug

--- client/src/components/feed/Share.tsx
+++ client/src/components/feed/Share.tsx
@@ -8,6 +8,7 @@ import { uploadPost } from '../../api/postAPI'
 import { v4 as uuid } from 'uuid'
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 import { storage } from '../../firebase'
+import LoadAnimation from '../load/LoadAnimation'
 // import LoadAnimation from '../load/LoadAnimation'
 
 interface Props {
@@ -84,63 +85,65 @@ const Share: React.FC<Props> = ({ triggerReload }) => {
 
 
     return (
-        <div className='w-100 h-44 rounded-lg dark:bg-dark_feed_secondary shadow-lg bg-light_feed_secondary dark:text-navBar_Text text-black'>
-            <div className='p-3'>
-                <div className='flex items-center'
-                >
-                    <Avatar
-                        // src="https://mui.com/static/branding/companies/nasa-dark.svg"
-                        src={currUser?.profilePicture === "" ? `https://avatars.dicebear.com/api/initials/${currUser?.userName}.svg` : currUser?.profilePicture}
-                        alt=""
-                    // draggable="false"
-                    // className='object-cover rounded-full cursor-pointer w-12 h-12 mr-2'
-                    />
-                    <div className='flex justify-between w-full'>
-                        <input
-                            ref={desc}
-                            type="text"
-                            placeholder={"What's in your mind " + currUser?.userName + "?"}
-                            className='w-full focus:outline-none rounded-md dark:bg-navBar_secondary text-black dark:text-navBar_Text p-1.5' />
-                        <Tooltip
-                            TransitionComponent={Zoom}
-                            TransitionProps={{ timeout: 400 }}
-                            title="Photo/Video">
-                            <label
-                                htmlFor='image-upload'
-                                className='cursor-pointer p-1.5 hover:bg-red-600 transition-all duration-300 ease-out rounded-lg'>
-                                <PermMedia />
-                            </label>
-                        </Tooltip>
-                        <input
-                            // hidden input for image upload
-                            className='hidden'
-                            id='image-upload'
-                            type="file"
-                            onChange={(e) => {
-                                // @ts-ignore
-                                setFile(e.target?.files ? e.target.files[0] : undefined)
-                                // @ts-ignore
-                                const filee = e.target.files[0]
-                                if (filee !== undefined) {
-                                    console.log("hi")
-                                    if (filee.size > 3000000) {
-                                        alert("File can only be below 3 mb")
-                                        setFile(undefined)
-                                    }
-                                    else {
+        <>
+            {user.user ?
+                <div className='w-100 h-44 rounded-lg dark:bg-dark_feed_secondary shadow-lg bg-light_feed_secondary dark:text-navBar_Text text-black'>
+                    <div className='p-3'>
+                        <div className='flex items-center'
+                        >
+                            <Avatar
+                                // src="https://mui.com/static/branding/companies/nasa-dark.svg"
+                                src={currUser?.profilePicture === "" ? `https://avatars.dicebear.com/api/initials/${currUser?.userName}.svg` : currUser?.profilePicture}
+                                alt=""
+                            // draggable="false"
+                            // className='object-cover rounded-full cursor-pointer w-12 h-12 mr-2'
+                            />
+                            <div className='flex justify-between w-full'>
+                                <input
+                                    ref={desc}
+                                    type="text"
+                                    placeholder={"What's in your mind " + currUser?.userName + "?"}
+                                    className='w-full focus:outline-none rounded-md dark:bg-navBar_secondary text-black dark:text-navBar_Text p-1.5' />
+                                <Tooltip
+                                    TransitionComponent={Zoom}
+                                    TransitionProps={{ timeout: 400 }}
+                                    title="Photo/Video">
+                                    <label
+                                        htmlFor='image-upload'
+                                        className='cursor-pointer p-1.5 hover:bg-red-600 transition-all duration-300 ease-out rounded-lg'>
+                                        <PermMedia />
+                                    </label>
+                                </Tooltip>
+                                <input
+                                    // hidden input for image upload
+                                    className='hidden'
+                                    id='image-upload'
+                                    type="file"
+                                    onChange={(e) => {
                                         // @ts-ignore
-                                        uploadImage(filee)
-                                    }
-                                }
-                            }}
-                            accept='.png, .jpeg, .jpg' />
-                    </div>
-                </div>
-                <hr className='my-4' />
-                <div className='mt-4'>
-                    <div className='flex justify-evenly'>
-                        <div className='flex flex-wrap gap-2 items-center'>
-                            {/* <Tooltip
+                                        setFile(e.target?.files ? e.target.files[0] : undefined)
+                                        // @ts-ignore
+                                        const filee = e.target.files[0]
+                                        if (filee !== undefined) {
+                                            console.log("hi")
+                                            if (filee.size > 3000000) {
+                                                alert("File can only be below 3 mb")
+                                                setFile(undefined)
+                                            }
+                                            else {
+                                                // @ts-ignore
+                                                uploadImage(filee)
+                                            }
+                                        }
+                                    }}
+                                    accept='.png, .jpeg, .jpg' />
+                            </div>
+                        </div>
+                        <hr className='my-4' />
+                        <div className='mt-4'>
+                            <div className='flex justify-evenly'>
+                                <div className='flex flex-wrap gap-2 items-center'>
+                                    {/* <Tooltip
                                 TransitionComponent={Zoom}
                                 TransitionProps={{ timeout: 400 }}
                                 title="Photo/Video">
@@ -150,7 +153,7 @@ const Share: React.FC<Props> = ({ triggerReload }) => {
                                     <PermMedia />
                                 </label>
                             </Tooltip> */}
-                            {/* <input
+                                    {/* <input
                                 // hidden input for image upload
                                 className='hidden'
                                 id='image-upload'
@@ -173,8 +176,8 @@ const Share: React.FC<Props> = ({ triggerReload }) => {
                                     }
                                 }}
                                 accept='.png, .jpeg, .jpg' /> */}
-                            {/* <span>Photo or Video</span> */}
-                            {/* <Tooltip
+                                    {/* <span>Photo or Video</span> */}
+                                    {/* <Tooltip
                                 TransitionComponent={Zoom}
                                 TransitionProps={{ timeout: 400 }}
                                 title="Tag">
@@ -202,45 +205,51 @@ const Share: React.FC<Props> = ({ triggerReload }) => {
                             </Tooltip> */}
 
 
-                            {/* <Tooltip
+                                    {/* <Tooltip
                                 TransitionComponent={Zoom}
                                 TransitionProps={{ timeout: 400 }}
                                 title="Share"> */}
-                            {
-                                !upload ?
-                                    imageURL !== "" &&
-                                    // <img src={imageURL} />
-                                    <img src={imageURL} alt="" className='h-10 w-auto' />
-                                    :
-                                    <>
-                                        <CircularProgress />
-                                    </>
-                            }
+                                    {
+                                        !upload ?
+                                            imageURL !== "" &&
+                                            // <img src={imageURL} />
+                                            <img src={imageURL} alt="" className='h-10 w-auto' />
+                                            :
+                                            <>
+                                                <CircularProgress />
+                                            </>
+                                    }
 
 
-                            {!upload ?
-                                <button
-                                    onClick={(e) => handleSubmit(e)}
-                                    className='bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded'>
-                                    {/* <Shortcut /> */}
-                                    Post
-                                </button>
-                                :
-                                <div>
-                                    Loading
+                                    {!upload ?
+                                        <button
+                                            onClick={(e) => handleSubmit(e)}
+                                            className='bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded'>
+                                            {/* <Shortcut /> */}
+                                            Post
+                                        </button>
+                                        :
+                                        <div>
+                                            Loading
+                                        </div>
+                                    }
+                                    {/* </Tooltip> */}
+
+
                                 </div>
-                            }
-                            {/* </Tooltip> */}
 
 
+
+                            </div>
                         </div>
-
-
-
                     </div>
                 </div>
-            </div>
-        </div>
+                :
+                <div className='w-100 h-44 flex items-center justify-center'>
+                    <LoadAnimation />
+                </div>
+            }
+        </>
     )
 }
 
---
+++ client/src/components/load/BarLoad.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BarLoader } from 'react-spinners'
+
+const BarLoad: React.FC = () => {
+    return (
+        <div>
+            <BarLoader color="rgb(37 99 235)" />
+        </div>
+    )
+}
+
+export default BarLoad
