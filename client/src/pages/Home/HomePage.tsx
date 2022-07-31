b740fa47999eea47898e10204b5701bce9de3872
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-07-31 12:31:03 +0530

    refactor home page

--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -18,9 +18,9 @@ const HomePage: React.FC = () => {
             <div>
                 <TopBar />
             </div>
-            <div className='flex h-[calc(100vh-56px)] w-full '>
+            <div className='flex flex-col lg:flex-row md:flex-row h-[calc(100vh-56px)] w-full '>
                 <div
-                    className='hidden md:block lg:block w-1/3'>
+                    className='w-full h-1/5 lg:h-full md:h-full lg:w-1/3 md:w-1/3'>
                     <SideBar />
                 </div>
                 <div className='w-full '>
