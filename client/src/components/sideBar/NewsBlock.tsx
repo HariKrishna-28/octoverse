bef83ddaa58b53bf87aac989a14d188974d71b68
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-08-21 18:44:07 +0530

    bug fix

--- client/src/components/sideBar/NewsBlock.tsx
+++ client/src/components/sideBar/NewsBlock.tsx
@@ -8,16 +8,16 @@ interface Props {
 
 const NewsBlock: React.FC<Props> = ({ news }) => {
     return (
-        <div className='dark:list__cards__dark list__cards__light transition-all duration-200 ease-out my-1'>
-            <a href={news.link} target="_blank" rel="noopener noreferrer">
+        <a href={news.link} target="_blank" rel="noopener noreferrer">
+            <div className='dark:list__cards__dark list__cards__light transition-all duration-200 ease-out my-1'>
                 <div className='text-sm text-blue-600'>
                     {moment(news.published_date).fromNow()}
                 </div>
                 <span className='text-sm font-bold'>
                     {news.title}
                 </span>
-            </a>
-        </div>
+            </div>
+        </a>
     )
 }
 
