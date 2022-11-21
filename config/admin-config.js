d1f4d4bfa9e5ccb19370a34e45337e87d9f705b0
Harikrishna-28 <harikrishnac2002@gmail.com>
2022-11-21 15:23:05 +0530

    add admin-token

---
+++ config/admin-config.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const admin_config = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+    : undefined,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
+module.exports = admin_config;
---
+++ config/firebase-config.js
@@ -0,0 +1,8 @@
+const admin = require("firebase-admin");
+
+// const serviceAccount = require("./firebase_admin_key.json");
+const serviceAccount = require("./admin-config");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
